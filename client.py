be40c89810068361356da51054b9abba5ad39f4e
kaushiksk <kaushikskalmady@gmail.com>
2017-11-23 20:19:31 +0530

    Improved interface. Prevent overwriting text

--- client.py
+++ client.py
@@ -1,54 +1,67 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : client.py
+# Author            : Kaushik S Kalmady
+# Date              : 17.11.2017
+# Last Modified Date: 17.11.2017
+# Last Modified By  : Kaushik S Kalmady
 import socket
 import sys
 import select
 
 buffer_size = 2048
 
-def chatclient():
 
-    host = socket.gethostname()
+def chatclient():
+    # host = socket.gethostname()
+    host = 'localhost'
     port = int(raw_input("Enter Port:"))
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(2)
-
+    global nick
     try:
-        s.connect((host,port))
+        s.connect((host, port))
     except:
-        print "Unable to Connect"
+        sys.stdout.write("Unable to Connect")
         sys.exit()
 
-    print "Connected successfully"
+    sys.stdout.write("Connected successfully\n")
     nick = raw_input("Choose Nick:")
     s.send(nick)
-    
-    print "["+nick+"]: ",;sys.stdout.flush()
 
-
+    sys.stdout.write("[" + nick + "]:")
+    sys.stdout.flush()
+    flag = 1
 
     while 1:
+        sock_list = [0, s]
 
-        sock_list = [sys.stdin,s]
-
-        read,write,err = select.select(sock_list,[],[])
+        read, write, err = select.select(sock_list, [], [])
 
         for sock in read:
+            if sock == 0:
 
-            if sock == s:
+                text = sys.stdin.readline().strip()
+                if(text[:7] == "%change"):
+                    nick = text.split()[-1]
+                if text:
+                    s.send(text)
+
+                sys.stdout.write(" "*(len(nick) + 3))
+                sys.stdout.flush()
+
+            elif sock == s:
 
                 data = s.recv(buffer_size)
                 if not data:
-                    print "\r"+"Disconnected"
+                    sys.stdout.write("\r"+"Disconnected")
                     sys.exit()
-
-                print data
-                print "["+nick+"]: ",;sys.stdout.flush()
-
-            else:
-
-                text = raw_input()
-                s.send(text)
-                print "\b["+nick+"]: ",;sys.stdout.flush()
+                else:
+                    sys.stdout.write(data + "\n")
+                    sys.stdout.flush()
+                    sys.stdout.write("[" + nick + "]:")
+                    sys.stdout.flush()
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     chatclient()
--- server.py
+++ server.py
@@ -1,10 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# File              : server.py
+# Author            : Kaushik S Kalmady
+# Date              : 17.11.2017
+# Last Modified Date: 18.11.2017
+# Last Modified By  : Kaushik S Kalmady
 import socket
 import sys
 import select
 import time
 
 
-host = ''
+host = 'localhost'
 port = 5550
 buffer_size = 2048
 sock_list = []
@@ -12,15 +19,15 @@ users = {}
 MAX_USERS = 10
 
 def chatserver():
-
-
+    """Summary
+    """
     server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server.bind((host,port))
     server.listen(MAX_USERS)
     server_up = time.strftime("%d/%h/%Y %H:%M:%S")
     sock_list.append(server)
-    print "Chat server started on port " + str(port)
+    print "Chat server started on port %s"%str(port)
     while 1:
 
         read, write, err = select.select(sock_list,[],[],0)
@@ -33,7 +40,7 @@ def chatserver():
                 sock_list.append(client)
 
                 print "["+time.strftime("%d/%h/%Y %H:%M:%S") + "] New Client Connected at (%s,%s)"%addr
-                sendtoall(server,client,"\r"+"New Client Connected: (%s,%s)"%addr)
+                sendtoall(server,client,"\r"+"New Client Connected: (%s,%s) "%addr)
                 nick = getnick(client)
                 sendtoall(server,client,"\r"+"Client (%s,%s) chose nick "%addr + nick)
 
@@ -53,8 +60,16 @@ def chatserver():
                         if data =="%quit":
                             removeclient(sock)
                             continue
+                        if data[:7] == "%change":
+                            newnick = data.split()[-1]
+                            changenick(sock,newnick)
 
-                        sendtoall(server,sock,"\r" + "[" + users[sock]["nick"] + "]:" + data)
+
+                            sendtoall(server,sock,"\r"+"Client (%s,%s) changed nick to "%addr + newnick)
+
+                            continue
+
+                        sendtoall(server,sock,"\n" + "[" + users[sock]["nick"] + "]:" + data)
 
                     else:
                         #consider as disconnection
@@ -67,7 +82,13 @@ def chatserver():
     server.close()
 
 def sendtoall(server,sock,data):
+    """Summary
 
+    Args:
+        server (TYPE): Description
+        sock (TYPE): Description
+        data (TYPE): Description
+    """
     for socket in sock_list:
 
         if socket!=server and socket!=sock:
@@ -80,13 +101,27 @@ def sendtoall(server,sock,data):
                     sock_list.remove(socket)
 
 def print_online_clients(server,client):
-    online = [users[i]["nick"]+"@"+str(i.getpeername())+" In: "+users[i]["in"] for i in sock_list if i!=server and i!=client]
+    """Summary
+
+    Args:
+        server (TYPE): Description
+        client (TYPE): Description
+    """
+    online = [users[i]["nick"]+"@"+str(i.getpeername())+" In: "+users[i]["in"] + "\n" for i in sock_list if i!=server and i!=client]
     if not online:
         online = ["None"]
     client.send("\r"+"Users currently online:\n"+"\n".join(online))
 
 
 def getnick(client):
+    """Summary
+
+    Args:
+        client (TYPE): Description
+
+    Returns:
+        TYPE: Description
+    """
     nick = client.recv(1024)
     #TODO check for duplicate nicks
     users[client] = {"nick":nick,
@@ -97,13 +132,35 @@ def getnick(client):
 
 
 def removeclient(sock):
+    """Summary
+
+    Args:
+        sock (TYPE): Description
+    """
     sock_list.remove(sock)
     print time.strftime("%d/%h/%Y %H:%M:%S") + ": "+  users[sock]["nick"] + " has disconnected"
     sendtoall(server,sock,"\r" + "Client " + users[sock]["nick"] + "(%s,%s) is now offline"%sock.getpeername())
 
 
 def sendinfo(server,sock):
+    """Summary
+
+    Args:
+        server (TYPE): Description
+        sock (TYPE): Description
+    """
     print_online_clients(server,sock)
+
+
+def changenick(client,newnick):
+    """Summary
+
+    Args:
+        client (TYPE): Description
+        newnick (TYPE): Description
+    """
+    users[client]["nick"] = newnick
+
 
 
 if __name__=="__main__":
